53adc11dc7f7bc27640d4aeee445ddac013e9545
cc-brayan-isaza <brayan.isaza@finoservices.com>
2024-04-30 17:23:20 -0500

    .

--- src/pages/Goals.jsx
+++ src/pages/Goals.jsx
@@ -55,7 +55,7 @@ export const Goals = () => {
         className={`${styles.innerWidth} mx-auto flex flex-col`}
       >
         <TypingText title="| Goals" textStyles="text-center text-white" />
-        <TitleText title={<>Insight about metaverse</>} textStyles="text-center" />
+        {/* <TitleText title={<>Insight about metaverse</>} textStyles="text-center" /> */}
         <div className="mt-[50px] flex flex-col gap-[30px]">
           {insights.map((item, index) => (
             <InsightCard key={`insight-${index}`} {...item} index={index + 1} />
